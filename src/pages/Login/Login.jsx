6de5bac310eef70956431198462a804b641a4aea
Ruben <rgolfe81@gmail.com>
2023-03-16 20:31:07 +0100

    feat: create hooks and handler for view page Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { InputText } from "../../common/InputText/InputText";
+import "./Login.css";
 
-const Login = () => {
+export const Login = () => {
+  const [credenciales, setCredenciales] = useState({
+    email: "",
+    password: "",
+  });
+  const [credencialesError, setCredencialesError] = useState({
+    emailError: "",
+    passwordError: "",
+  })
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+
+  const inputHandler = (e) => {
+    setCredenciales((preveState => ({...preveState, [e.target.name]: e.target.value,})));
+  };
+
+  
+  useEffect(()=>{
+    console.log("Credenciales vale : ", credenciales);
+}, [credenciales]);
+
   return (
-    <div>Login</div>
-  )
-}
-
-export default Login
+    <div className="loginDesign">
+      <div className="titleDesign">
+        <h2>Login</h2>
+      </div>
+      <InputText
+        type="email"
+        name="email"
+        placeholder="escribe un email"
+        changeFunction={(e) => inputHandler(e)}
+        validateFunction={(e) => inputValidate(e)}
+      />
+        <div>{credencialesError.emailError}</div>
+      <InputText
+        type="password"
+        name="password"
+        placeholder="escribe el password"
+        changeFunction={(e) => inputHandler(e)}
+        validateFunction={(e) => inputValidate(e)}
+      />
+      <div>{credencialesError.passwordError}</div>
+      <div className={(isEmailValid && isPasswordValid) ? "buttonOn" : "buttonOff" } /*onClick={() => loginFunction}*/>Login</div>
+    </div>
+  );
+};
